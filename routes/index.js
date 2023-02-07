df5a35c49e0f7b435de8fddbc3737e5183d7e3d1
Zsofia Oroszi <51414584+zsorosz@users.noreply.github.com>
2023-02-07 19:45:49 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 const router = require("express").Router();
 
-// const {isLoggedIn} = require('../middleware/route-guard');
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -12,18 +11,6 @@ router.get("/profile", (req, res, next) => {
 
   res.render("profile", {user:req.session.user});
 });
-
-// /* GET loggedOut route */
-// router.get("/main", isLoggedIn ,(req, res, next) => {
-
-//   res.render("main");
-// });
-
-// /* GET  loggedIn route */
-// router.get("/private", isLoggedIn, (req, res, next) => {
-
-//   res.render("private");
-// });
 
 
 
