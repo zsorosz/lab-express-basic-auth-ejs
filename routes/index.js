80bb1eac3a079a8f574e2c6a67baef02c5d8ba33
zsorosz <oroszi.zsofi@gmail.com>
2023-02-07 20:27:56 +0100

    Iteration-3 done

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require("express").Router();
-
+const { isLoggedIn } = require("../middleware/route-guard");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
@@ -8,10 +8,15 @@ router.get("/", (req, res, next) => {
 
 /* GET  profile page */
 router.get("/profile", (req, res, next) => {
-
-  res.render("profile", {user:req.session.user});
+  res.render("profile", { user: req.session.user });
 });
 
+router.get("/main", isLoggedIn, (req, res, next) => {
+  res.render("main");
+});
 
+router.get("/private", isLoggedIn, (req, res, next) => {
+  res.render("private");
+});
 
 module.exports = router;
